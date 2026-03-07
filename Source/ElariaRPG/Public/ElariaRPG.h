ff7dad5c688de11e200c91bbe52a7f30f91a5295
Trapezze <rockmark92@mail.ru>
2026-03-07 14:10:29 +1000

    fix: restore LogElaria and define shooter log categories

--- Source/ElariaRPG/Public/ElariaRPG.h
+++ Source/ElariaRPG/Public/ElariaRPG.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "CoreMinimal.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogCombat, Log, All);
+// Старую категорию возвращаем временно, чтобы не блокировать сборку
+DECLARE_LOG_CATEGORY_EXTERN(LogElaria, Log, All);
 
+// Новые категории (для шутера)
+DECLARE_LOG_CATEGORY_EXTERN(LogCombat, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogWeapons, Log, All);
